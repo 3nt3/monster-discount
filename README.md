5e4bce3aa693b1f9fd0c1027437db018ed8fa35c
3nt3 <gott@3nt3.de>
2022-05-21 18:51:01 +0200

    lol
    Signed-off-by: 3nt3 <gott@3nt3.de>

---
+++ README.md
@@ -0,0 +1,3 @@
+# monster discount
+
+Find products that are discounted at REWE from the command line
--- scrape.py
+++ scrape.py
@@ -17,14 +17,21 @@ url = "https://www.rewe.de/angebote/haan/1940156/rewe-markt-dieker-str-101/"
 driver.get(url)
 
 titles = driver.find_elements(By.CLASS_NAME, "cor-offer-title")
+found = False
 for title in titles:
     if sys.argv[1].lower() in title.text.lower():
-        parent = title.find_element(By.XPATH, '..')
-        percent = parent.find_element(By.CLASS_NAME,
-                                      "cor-offer-price-label").text
-        price = parent.find_element(By.CLASS_NAME,
-                                    "cor-offer-price-amount").text
-        print(
-            f"{title.text} if discounted to {Fore.YELLOW}{Back.RED}{price}{Style.RESET_ALL} {Back.YELLOW}{Fore.RED}({percent})")
+        try:
+            parent = title.find_element(By.XPATH, '..')
+            percent = parent.find_element(By.CLASS_NAME,
+                                          "cor-offer-price-label").text
+            price = parent.find_element(By.CLASS_NAME,
+                                        "cor-offer-price-amount").text
+            print(
+                f"{title.text} is discounted to {Fore.YELLOW}{Back.RED}{price}{Style.RESET_ALL} {Back.YELLOW}{Fore.RED}({percent}){Style.RESET_ALL}")
+            found = True
+        except Exception as e:
+            pass
 
-driver.close()
+if not found:
+    driver.close()
+    print(f"'{sys.argv[1]}' is not discounted")
