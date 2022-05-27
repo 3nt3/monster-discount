46fa8242f1a86a4391388e005fc854c7e98529da
3nt3 <gott@3nt3.de>
2022-05-27 19:18:15 +0200

    read database
    Signed-off-by: 3nt3 <gott@3nt3.de>

--- scraper/src/main.rs
+++ scraper/src/main.rs
@@ -1,4 +1,8 @@
+use fcm::{Client, MessageBuilder};
+use sqlx::postgres::PgPoolOptions;
+use sqlx::{Executor, Pool, Postgres};
 use std::collections::HashMap;
+use std::env;
 
 use serde::Deserialize;
 
@@ -32,12 +36,39 @@ struct PriceData {
 
 #[tokio::main]
 async fn main() {
-    let client = reqwest::Client::new();
-    let resp = client
-        .get("https://mobile-api.rewe.de/api/v3/all-offers?marketCode=1940156")
-        .header("User-Agent", "Dart/2.16.2 (dart:io)")
-        .send()
+    dotenv::dotenv();
+    let database_url = env::var("DATABASE_URL").unwrap();
+
+    let pool = PgPoolOptions::new()
+        .max_connections(5)
+        .connect(&database_url)
         .await
         .unwrap();
-    println!("{:#?}", resp.json::<Response>().await);
+
+    let mut markets: HashMap<i32, Vec<String>> = HashMap::new();
+    let db_data = sqlx::query!("SELECT token, market_id FROM token__market")
+        .fetch_all(&pool)
+        .await
+        .unwrap();
+
+    for item in &db_data {
+        if let None = markets.get(&item.market_id) {
+            markets.insert(item.market_id, vec![(&item.token).to_string()]);
+        } else {
+            markets
+                .get_mut(&item.market_id)
+                .unwrap()
+                .push((&item.token).to_string());
+        }
+    }
+    for (market, tokens) in markets.iter() {
+        let client = reqwest::Client::new();
+        let builder = client
+            .get("https://mobile-api.rewe.de/api/v3/all-offers")
+            .query(&[("marketCode", market)])
+            .header("User-Agent", "Dart/2.16.2 (dart:io)");
+        dbg!(&builder);
+        let resp = builder.send().await.unwrap();
+        dbg!(resp.status());
+    }
 }
