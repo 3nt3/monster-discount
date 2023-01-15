a14bbeaf282ef577ab501d7cc9bd1c739aa92a70
3nt3 <gott@3nt3.de>
2023-01-15 02:22:41 +0100

    add trinkgut lol
    Signed-off-by: 3nt3 <gott@3nt3.de>

--- scraper/src/trinkgut/scrape.rs
+++ scraper/src/trinkgut/scrape.rs
@@ -1,8 +1,6 @@
 use crate::trinkgut::models::Listing;
 use scraper::{Html, Selector};
 
-use std::fs;
-
 pub async fn get_listings(market_name: String) -> anyhow::Result<Vec<Listing>> {
     let text = reqwest::get(format!("https://www.trinkgut.de/markt/{market_name}"))
         .await?
@@ -10,8 +8,6 @@ pub async fn get_listings(market_name: String) -> anyhow::Result<Vec<Listing>> {
         .await?;
 
     let document = Html::parse_document(&text);
-
-    fs::write("/tmp/test.html", &text).unwrap();
 
     let market_link_selector = Selector::parse("a.market-link--set-default").unwrap();
     let market_link = document
@@ -35,23 +31,35 @@ async fn get_listings_by_market_id(market_id: String) -> anyhow::Result<Vec<List
         .send()
         .await?;
 
-    // dbg!(&text);
-    //
-    dbg!(&response);
     let text = response.text().await?;
 
     let document = Html::parse_document(&text);
-    fs::write("/tmp/test.html", &text).unwrap();
 
     let product_selector = Selector::parse(".product--info").unwrap();
-
     let title_selector = Selector::parse(".product--title").unwrap();
     let price_selector = Selector::parse(".product--price").unwrap();
+    let image_selector = Selector::parse("img").unwrap();
 
     for element in document.select(&product_selector) {
-        let title = element.select(&title_selector).next().unwrap();
-        let price = element.select(&price_selector).next().unwrap();
-        println!("{}, {}", title.text().collect::<Vec<_>>().join("").replace("\n", ""), price.text().collect::<Vec<_>>().join("").replace("\n", ""));
+        let maybe_title = element.select(&title_selector).next();
+        let maybe_price = element.select(&price_selector).next();
+        let maybe_image = element.select(&image_selector).next();
+
+        match (maybe_title, maybe_price, maybe_image) {
+            (Some(title), Some(price), Some(image)) => {
+                let title_text = title.text().collect::<Vec<_>>().join("").trim().to_string();
+                let price_text = price.text().collect::<Vec<_>>().join("").trim().to_string();
+
+                let maybe_image_url = image.value().attr("srcset");
+                if let None = maybe_image_url {
+                    continue;
+                }
+                let image_url = maybe_image_url.unwrap();
+
+                println!("{} ({}): {}", title_text, image_url, price_text);
+            }
+            _ => {}
+        }
     }
 
     Ok(vec![])
